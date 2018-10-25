d847e34b33d023e50f193f3575d3897e11f4515a
James Shadrake <jimkarts@hotmail.co.uk>
2018-10-25 09:31:52 +0000

    Questions and answer dictionary set up

--- assets/python/quiz.py
+++ assets/python/quiz.py
@@ -1,30 +1,102 @@
-questions = [
-    "1. Which planet is nearest to the sun?",
-    "2. What is the largest planet in the solar system?",
-    "3. Between which two planets do you find the asteroid belt?",
-    "4. What is the name of the largest object in the asterid belt?",
-    "5. Now pluto has been downgraded as a planet, which planet is the furthest from the sun?",
-    "6. How many planets are there?",
-    "7. How many of these are gas giants?",
-    "8. Umbriel is a moon of which planet?",
-    "9. How many moons orbit Mars?",
-    "10. Can you name one of them?",
-    "11. Pluto is the largest object in which part of the solar system?",
-    "12. What is the name of Pluto's moon?",
-    "13. Which planet is famous for its large rings?",
-    "14. Name one of the moons of Mars?",
-    "15. After the sun, which is the nearest star to the sun?",
-    "16. The solar system is a part of which galaxy?",
-    "17. Approximately how old is the solar system thought to be?",
-    "18. When the moon passes across the sun and blocks the daylight, this is called a solar...",
-    "19. Makemake is part of the asteroid belt. True or false?",
-    "20. Which of these is a comet?"
+solar_system_questions = [
+    {
+        "1.": "Which planet is nearest to the sun?", 
+        "choices": {"a. ": "Venus", "b. ": "Mercury", "c. ": "Pluto"},
+        "Answer": "b"
+    },
+    {
+        "2.": "What is the largest planet in the solar system?",
+        "choices": {"a. ": "Jupiter", "b. ": "Uranus", "c. ": "Saturn"},
+        "Answer": "a"
+    },
+    {
+        "3.": "Between which two planets do you find the asteroid belt?",
+        "choices": {"a. ": "Earth and Mars", "b. ": "Mars and Jupiter", "c. ": "Jupiter and Saturn"},
+        "Answer": "b"
+    },
+    {
+        "4.": "What is the name of the largest object in the asterid belt?",
+        "choices": {"a. ": "Ceres", "b. ": "Juno", "c. ": "Hygiea"},
+        "Answer": "a"
+    },
+    {
+        "5.": "Now pluto has been downgraded as a planet, which planet is the furthest from the sun?",
+        "choices": {"a. ": "Uranus", "b. ": "Neptune", "c. ": "Mars"},
+        "Answer": "b"
+    },
+    {
+        "6.": "How many planets are there?",
+        "choices": {"a. ": "Eight", "b. ": "Six", "c. ": "Seven"},
+        "Answer": "a"
+    },
+    {
+        "7.": "How many of these are gas giants?",
+        "choices": {"a. ": "Five", "b. ": "Three", "c. ": "Four"},
+        "Answer": "c"
+    },
+    {
+        "8.": "Umbriel is a moon of which planet?",
+        "choices": {"a. ": "Saturn", "b. ": "Neptune", "c. ": "Uranus"},
+        "Answer": "c"
+    },
+    {
+        "9.": "How many moons orbit Mars?",
+        "choices": {"a. ": "One", "b. ": "Two", "c. ": "Three"},
+        "Answer": "b"
+    },
+    {
+        "10.": "Which of these is a moon of Mars?",
+        "choices": {"a. ": "Makemake", "b. ": "Europa", "c. ": "Phobos"},
+        "Answer": "c"
+    },
+    {
+        "11.": "Pluto is the largest object in which part of the solar system?",
+        "choices": {"a. ": "Kuiper Belt", "b. ": "Inner Planets", "c. ": "Oort Cloud"},
+        "Answer": "a"
+    },
+    {
+        "12.": "Which of these is not one of Plutos moons?",
+        "choices": {"a. ": "Hydra", "b. ": "Io", "c. ": "Nix"},
+        "Answer": "b"
+    },
+    {
+        "13.": "Which planet is famous for its large rings?",
+        "choices": {"a. ": "Venus", "b. ": "Neptune", "c. ": "Saturn"},
+        "Answer": "c"
+    },
+    {
+        "14.": "Which of the planets is thought to have the warmest climate?",
+        "choices": {"a. ": "Mars", "b. ": "Venus", "c. ": "Mercury"},
+        "Answer": "b"
+    },
+    {
+        "15.": "After the sun, which is the nearest star to the solar system at four light years?",
+        "choices": {"a. ": "Proxima Centauri", "b. ": "Barnards Star", "c. ": "Centauri A"},
+        "Answer": "a"
+    },
+    {
+        "16.": "The solar system is a part of which galaxy?",
+        "choices": {"a. ": "The Sombrero Galaxy", "b. ": "Andromeda", "c. ": "Milky Way"},
+        "Answer": "c"
+    },
+    {
+        "17.": "Approximately how old is the solar system thought to be?",
+        "choices": {"a. ": "6 Billion Years", "b. ": "4.5 Billion Years", "c. ": "3 Billion Years"},
+        "Answer": "b"
+    },
+    {
+        "18.": "When the moon passes across the sun and blocks the daylight, this is called a solar...",
+        "choices": {"a. ": "Eclipse", "b. ": "Storm", "c. ": "Flair"},
+        "Answer": "b"
+    },
+    {
+        "19.": "Makemake is part of the asteroid belt. True or false?",
+        "choices": {"a. ": "True", "b. ": "False", "c": "There is no such object"},
+        "Answer": "b"
+    },
+    {
+        "20.": "Which of these is a comet?",
+        "choices": {"a. ": "Halley's", "b. ": "Eris", "c. ": "Callisto"},
+        "Answer": "a"
+    }
 ]
-
-score = 0
-
-def ask_question(self, prompt):
-    for question in questions:
-        print question[0]
-        
-solar_system_quiz(questions)

071ba2c546c78d621473ee2ee526f19697e2815e
James Shadrake <jimkarts@hotmail.co.uk>
2018-11-01 14:47:26 +0000

    base.html, templates folder set up and run.py file initialised

---
+++ run.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+    
+@app.route("/solar-info")
+def solar_info():
+    return render_template("solar-info.html")
+    
+@app.route("/solar-quiz")
+def solar_quiz():
+    return render_template("solar-quiz.html")
+    
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True)

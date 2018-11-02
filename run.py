22d3dc8d6ec4a992b3b927843b2b2a05fb96d21b
James Shadrake <jimkarts@hotmail.co.uk>
2018-11-02 20:00:13 +0000

    Functionality of solar info page now working

--- run.py
+++ run.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_heading="Welcome")
     
 @app.route("/solar-info")
 def solar_info():
-    return render_template("solar-info.html")
+    data = []
+    with open("data/solar-bodies-info.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("solar-info.html", page_heading="Solar System", solar_bodies_data=data)
     
 @app.route("/solar-quiz")
 def solar_quiz():
-    return render_template("solar-quiz.html")
+    return render_template("solar-quiz.html", page_heading="Solar Quiz")
     
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),

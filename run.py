4b1bef915d7d5b4bab7d3786eaa47c4196dca55c
James Shadrake <jimkarts@hotmail.co.uk>
2019-02-08 17:00:05 +0000

    run.py updated following discussion with tutor. Quiz now working prior to writing to names.txt

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.secret_key = "username_collected"
 
 
+
 """Calling the data from questions.json and putting it into a variable called questions"""
 questions = []
 with open("data/questions.json", "r") as json_data:
@@ -37,6 +38,7 @@ def fetch_names_to_show():
 """Rendering the home page"""
 @app.route("/")
 def index():
+    session.clear()
     return render_template("index.html", page_heading="Welcome")
   
   
@@ -44,6 +46,7 @@ def index():
 """Rendering the solar info page"""   
 @app.route("/solar_info")
 def solar_info():
+    session.clear()
     data = []
     with open("data/solar-bodies-info.json", "r") as json_data:
         data = json.load(json_data)
@@ -60,10 +63,14 @@ def solar_quiz():
         username = request.form["username"]
         firstname = request.form["firstname"]
         lastname = request.form["lastname"]
+        #Set the score variable to be used in get_question to 0
+        score = 0
+        
         """Creating the sessions for the variables"""
         session['username'] = username
         session['firstname'] = firstname
         session['lastname'] = lastname
+        session['score'] = score
         """Setting the Variables"""
         flash("Thanks for playing {0}. Your username is {1}, so be sure to look for it on the leaderboard at the end".format(request.form["firstname"], request.form["username"]))
         write_to_file("data/names.txt", request.form["firstname"] + " " + request.form["lastname"] + " - Username: " + request.form["username"] + "\n")
@@ -73,10 +80,10 @@ def solar_quiz():
     
 
 
+
 """Iterating through the questions from the solar-bodies-info.json file"""    
 @app.route("/solar_quiz/<username>/question/<int:id>", methods=["GET", "POST"]) #How to put the button in this part of the file to iterate through the questions?
 def get_question(username, id):
-    #Set the score variable to 0
     questions = []
     print(request.form.items()) #Mentor suggestion to show in console below
     with open("data/questions.json", "r") as json_data:
@@ -86,20 +93,21 @@ def get_question(username, id):
         a = questions[id - 1]["answer"]
         for q in questions:
             if request.method == "POST":
-                score = 0
-                session['score'] = score
+                answer_given = request.form["choice"] #Variable answer_given finds which was selected in questions.py
+                """function to increase value of score by 1 if correct_answer is the same as answer_given"""
+                #def add_score(score, a): #create the function with score as an argument - currently set at 0
+                if answer_given.lower() == a.lower(): #Check whether answer given and correct answer are the same
+                    session['score'] += 1 #if so add 1
+                print(answer_given.lower())
+                print(a.lower())
+                print(session['score'])
+                #return score #Return the final score when all answers added together
                 id += 1
                 if id > len(questions):
                     return redirect(url_for("quiz_completed", page_heading="Quiz Completed", username=session['username'], firstname=session['firstname'], lastname=session['lastname'], score=session['score']))
-                answer_given = request.form["choice"] #Variable answer_given finds which was selected in questions.py
-                """function to increase value of score by 1 if correct_answer is the same as answer_given"""
-                def add_score(): #create the function with score as an argument - currently set at 0
-                    if answer_given == a: #Check whether answer given and correct answer are the same
-                        score += 1 #if so add 1
-                    return score #Return the final score when all answers added together
                 return redirect(url_for("get_question", username=session['username'], firstname=session['firstname'], lastname=session['lastname'], score=session['score'], id=id))
                 
-            return render_template("questions.html", page_heading="Quiz Questions", question=questions[id - 1], username=session['username'], firstname=session['firstname'], lastname=session['lastname'])
+            return render_template("questions.html", page_heading="Quiz Questions", question=questions[id - 1], username=session['username'], firstname=session['firstname'], lastname=session['lastname'], score=session['score'], id=id)
   
 
 
@@ -107,20 +115,21 @@ def get_question(username, id):
 @app.route("/solar_quiz/username/quiz_completed", methods=["GET", "POST"])
 def quiz_completed():
     if session['score'] == 20:
-        flash("Thanks for playing {0}. You scored {1}/20. Well done, that is a perfect score! See yourself on the leaderboard.".format(session['username'], session['score']))
+        flash("Thanks for playing {0}. You scored {1}/{2}. Well done, that is a perfect score! See yourself on the leaderboard.".format(session['username'], session['score'], len(questions)))
     elif session['score'] >= 15:
-        flash("Thanks for playing {0}. You scored {1}/20. That's a great score! See where you stand on the leaderboard.".format(session['username'], session['score']))
-    elif session['score'] <= 10:
-        flash("Thanks for playing {0}. You scored {1}/20. That's a decent score! See where you stand on the leaderboard, or look at the info.".format(session['username'], session['score']))
+        flash("Thanks for playing {0}. You scored {1}/{2}. That's a great score! See where you stand on the leaderboard.".format(session['username'], session['score'], len(questions)))
+    elif session['score'] >= 10:
+        flash("Thanks for playing {0}. You scored {1}/{2}. That's a decent score! See where you stand on the leaderboard, or look at the info.".format(session['username'], session['score'], len(questions)))
     else:
-        flash("Thanks for playing {0}. You scored {1}/20. Have a look at the leaderboard, or the Solar Info page if you'd like to learn more about the solar system.".format(session['username'], session['score']))
-    return render_template("quiz-completed.html", page_heading="Quiz Completed", username=session['username'], firstname=session['firstname'], lastname=session['lastname'], score=session['score']) 
+        flash("Thanks for playing {0}. You scored {1}/{2}. Have a look at the leaderboard, or the Solar Info page if you'd like to learn more about the solar system.".format(session['username'], session['score'], len(questions)))
+    return render_template("quiz-completed.html", page_heading="Quiz Completed", username=session['username'], firstname=session['firstname'], lastname=session['lastname'], score=session['score'])
     
    
     
 """Post quiz leaderboard rendering"""
 @app.route("/solar_quiz/leaderboard")
 def leaderboard():
+    session.clear()
     """Display the names from the text file"""
     show_names = fetch_names_to_show()
     return render_template("leaderboard.html", page_heading="Current Leaderboard", names=show_names)
